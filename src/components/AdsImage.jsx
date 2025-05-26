c21da0bc7ef077f1342b52a8a8781c662697fa05
Matias Velozo <matias.velozo.4c@gmail.com>
2025-05-25 21:54:36 -0300

    redirigiendo a goPremium

--- src/components/AdsImage.jsx
+++ src/components/AdsImage.jsx
@@ -1,8 +1,13 @@
-import { useState, useEffect } from "react"
-import { View, Image, StyleSheet, TouchableOpacity } from "react-native"
+import { useState, useEffect } from "react";
+import { View, Image, StyleSheet, TouchableOpacity } from "react-native";
+import { useRouter } from "expo-router";
 
 const AdsImage = ({ onPress, style }) => {
-  const [currentAd, setCurrentAd] = useState(null)
+  const [currentAd, setCurrentAd] = useState(null);
+  const router = useRouter();
+  const handleGoPremium = () => {
+    router.push("/tabs/goPremium");
+  };
 
   const adsImages = [
     require("../assets/ads/ads1.jpeg"),
@@ -10,55 +15,55 @@ const AdsImage = ({ onPress, style }) => {
     require("../assets/ads/ads3.jpeg"),
     require("../assets/ads/ads4.jpeg"),
     require("../assets/ads/ads5.jpeg"),
-  ]
+  ];
 
   const getRandomAd = () => {
-    const randomIndex = Math.floor(Math.random() * adsImages.length)
-    return adsImages[randomIndex]
-  }
+    const randomIndex = Math.floor(Math.random() * adsImages.length);
+    return adsImages[randomIndex];
+  };
 
   useEffect(() => {
-    setCurrentAd(getRandomAd())
-  }, [])
+    setCurrentAd(getRandomAd());
+  }, []);
 
   const changeAd = () => {
-    setCurrentAd(getRandomAd())
-  }
+    setCurrentAd(getRandomAd());
+  };
 
   if (!currentAd) {
-    return null 
+    return null;
   }
 
   const AdContent = (
     <View style={[styles.container, style]}>
       <Image source={currentAd} style={styles.image} resizeMode="cover" />
     </View>
-  )
+  );
 
   if (onPress) {
     return (
-      <TouchableOpacity onPress={onPress} activeOpacity={0.9}>
+      <TouchableOpacity onPress={handleGoPremium} activeOpacity={0.9}>
         {AdContent}
       </TouchableOpacity>
-    )
+    );
   }
 
-  return AdContent
-}
+  return AdContent;
+};
 
 const styles = StyleSheet.create({
   container: {
-    marginTop:20,
-    marginBottom:20,
+    marginTop: 20,
+    marginBottom: 20,
     width: "100%",
     height: 70,
     overflow: "hidden",
-    backgroundColor: "#000", 
+    backgroundColor: "#000",
   },
   image: {
     width: "100%",
     height: "100%",
   },
-})
+});
 
-export default AdsImage
+export default AdsImage;
